63352fda3c49c8c9331c7749d47c090500602664
thuegens <T.P.R.huegens@student.tudelft.nl>
2024-06-07 15:46:12 +0200

    started to integrate test_plotting.mlx into loop - missing topography data

--- GSH/IterativeLoop.m
+++ GSH/IterativeLoop.m
@@ -20,25 +20,43 @@ PlanetaryModel
 
 latLim =    [-89.5 89.5 1];  % [deg] min latitude, max latitude, resolution latitude (preferable similar to latitude)
 lonLim =    [-180 180 1];% [deg] min longitude, max longitude, resolution longitude (preferable similar to latitude)
-height =    225000.0; % height of computation above spheroid
+height =    0.0; % height of computation above spheroid
 SHbounds =  [0 49]; % Truncation settings: lower limit, upper limit SH-coefficients used
 
 %%%%%%%%%%%%%% Part that can be modified %%%%%%%%%%%%%%%%%%%%%%%
 
+%% Reference Model
+
+
 %% Global Spherical Harmonic Analysis 
 
+% new
 tic;
+Model.l1.bound = [HOME '/GSH/Data/MercuryCrust/crust_bd_new.gmt'];
 [V] = model_SH_analysis(Model);
 toc
+save([HOME '/GSH/Results/' Model.name '_V_new.mat'],'V')
 
-save([HOME '/GSH/Results/' Model.name '_V.mat'],'V')
+% low
+tic;
+Model.l1.bound = [HOME '/GSH/Data/MercuryCrust/crust_bd_low.gmt'];
+[V] = model_SH_analysis(Model);
+toc
+save([HOME '/GSH/Results/' Model.name '_V_low.mat'],'V')
+
+% high
+tic;
+Model.l1.bound = [HOME '/GSH/Data/MercuryCrust/crust_bd_high.gmt'];
+[V] = model_SH_analysis(Model);
+toc
+save([HOME '/GSH/Results/' Model.name '_V_high.mat'],'V')
 
 %% Global Spherical Harmonic Synthesis
 
+% new
 tic;
+Model.l1.bound = [HOME '/GSH/Data/MercuryCrust/crust_bd_new.gmt'];
 [data] = model_SH_synthesis(lonLim,latLim,height,SHbounds,V,Model);
 toc
-
-%% Save data
-
 save([HOME '/GSH/Results/' Model.name '_' num2str(SHbounds(1)) '_' num2str(SHbounds(2)) '_data.mat'],'data')
+
